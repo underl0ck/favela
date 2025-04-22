413fc4a5d332d8876d1e9bee43e9b416e6c252e4
Underl0ck <fcardoso@ymail.com>
2025-04-22 18:05:05 +0200

    Setup CloudFlare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+/* #import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -7,4 +7,18 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'server', // Enable server-side rendering
+});
+*/
+
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare'; // <-- Add this line
+
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server', // Enable server-side rendering
+  adapter: cloudflare(), // <-- Add this line
 });
